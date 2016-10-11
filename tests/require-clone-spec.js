553944e88ef12182d8b067f67e965465280e4f99
Jonathan Wickens <jonathanwickens@Jonathans-MacBook-Pro.local>
2016-10-12 01:46:06 +0200

    fixed tests and not actually loading modules

--- tests/require-clone-spec.js
+++ tests/require-clone-spec.js
@@ -6,6 +6,8 @@ describe("require", function() {
   it("returns the same results when loading the same module twice", function() {
         var a = require("./test-module");
         var b = require("./test-module");
+        a.should.be.type('number');
+        b.should.be.type('number');
         a.should.equal(b);
   });
 });
@@ -15,12 +17,16 @@ describe("require-clone", function() {
     it("returns different results when loading the same module twice", function() {
         var a = requireClone("./test-module");
         var b = requireClone("./test-module");
+        a.should.be.type('number');
+        b.should.be.type('number');
         a.should.not.equal(b);
     });
 
     it("still returns different results when loading the same module though a parent module that requires it", function() {
         var a = requireClone("./parent-test-module");
         var b = requireClone("./parent-test-module");
+        a.should.be.type('number');
+        b.should.be.type('number');
         a.should.not.equal(b);
     });
 });
--- tests/test-module.js
+++ tests/test-module.js
@@ -1,3 +1,2 @@
-var random_num = Math.floor(Math.random() * 10);
-
+var random_num = Math.floor(Math.random() * 1000);
 module.exports = random_num;
